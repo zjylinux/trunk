551ddf902a0696731a36991ef6fb4bbbc1d158dc
zjylinux <jiaoyangzhang@163.com>
2021-12-27 18:10:13 +0800

    memcpy user to/from kernel

--- Linux/cdev/hello.c
+++ Linux/cdev/hello.c
@@ -3,31 +3,48 @@
 #include <linux/kernel.h>
 #include <linux/completion.h>
 #include <linux/fs.h>
+#include <linux/uaccess.h>
 
 #define NAME "myled"
 
 int major;
-
+char kbuf[128]={0};
 ssize_t myled_read (struct file *file, char __user *user, size_t size, loff_t *loff)
 {
+	int ret;
 	printk("myled_read\n");
-	return 0;
+	if (size >sizeof(kbuf)) size = sizeof(kbuf);
+	ret = copy_to_user(user,kbuf,size);
+	if (ret)
+	{
+		printk("copy data to user error");
+		return -EINVAL;
 	}
+	return size;
+}
 ssize_t myled_write (struct file *file, const char __user *user, size_t size, loff_t *loff)
 {
+	int ret;
 	printk("myled_write\n");
-	return 0;
+	if (size >sizeof(kbuf)) size = sizeof(kbuf);
+	ret = copy_from_user(kbuf,user,size);
+	if (ret)
+	{
+		printk("copy data from user error");
+		return -EINVAL;
 	}
-int myled_open (struct inode * inode, struct file * file)
+	return size;
+}
+int myled_open (struct inode *inode, struct file *file)
 {
 	printk("myled_open\n");
 	return 0;
-	}
+}
 int myled_close (struct inode *inode, struct file *filp)
 {
 	printk("myled_close\n");
 	return 0;
-	}
+}
 const struct file_operations fops = {
 	.open = myled_open,
 	.read = myled_read,
